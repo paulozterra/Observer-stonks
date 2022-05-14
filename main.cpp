2d9b25a9b5c10b76214c7a7d58cf9ddb72c30eb1
paulozterra <66433825+paulozterra@users.noreply.github.com>
2022-05-13 20:49:04 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,10 +14,10 @@ class DisplayDevice;
 class ObserverInterface {
 public:
     ObserverInterface() = default;
-    virtual void update(shared_ptr<SubjectInterface> s) = 0;
+    virtual void update(weak_ptr<SubjectInterface> s) = 0;
 };
 
-class SubjectInterface {
+class SubjectInterface : public std::enable_shared_from_this<SubjectInterface> {
 protected:
     forward_list<shared_ptr<ObserverInterface>> observers;
 public:
@@ -39,7 +39,7 @@ public:
     };
     virtual void notifyObserver() {
         for (auto &obs : observers) {
-            obs->update( shared_ptr<SubjectInterface>(this) );
+            obs->update(shared_from_this()); //( shared_ptr<SubjectInterface>(this) ); // https://stackoverflow.com/questions/11711034/stdshared-ptr-of-this
         }
     };
 
@@ -73,9 +73,9 @@ class StatisticsObserver : public ObserverInterface{
 private:
     string statistics;
 public:
-    void update(shared_ptr<SubjectInterface> s) override {
+    void update(weak_ptr<SubjectInterface> s) override {
       //auto ws = (WeatherStation*) (s);
-      auto ws = dynamic_pointer_cast<WeatherStation>(s); // Esto: https://en.cppreference.com/w/cpp/memory/shared_ptr/pointer_cast
+      auto ws = dynamic_pointer_cast<WeatherStation>(shared_ptr<SubjectInterface>(s)); // Esto: https://en.cppreference.com/w/cpp/memory/shared_ptr/pointer_cast
         statistics = "Hum:"+to_string(ws->getHumidity())+"\nTemp:"+to_string(ws->getTemperature())+"\nPres:"+to_string(ws->getPressure());
     }
     string getStatistics() {return statistics;}
@@ -85,8 +85,8 @@ class CurrentObserver : public ObserverInterface{
 private:
     string current;
 public:
-    void update(shared_ptr<SubjectInterface> s) override {
-        auto ws = dynamic_pointer_cast<WeatherStation>(s);
+    void update(weak_ptr<SubjectInterface> s) override {
+        auto ws = dynamic_pointer_cast<WeatherStation>(shared_ptr<SubjectInterface>(s));
         current = to_string(ws->getPressure()*0.8 + ws->getHumidity()*0.1 + ws->getTemperature()*0.1);
     }
     string getCurrent() {return current;}
@@ -96,8 +96,8 @@ class ForecastObserver : public ObserverInterface{
 private:
     string forecast;
 public:
-    void update(shared_ptr<SubjectInterface> s) override {
-        auto ws = dynamic_pointer_cast<WeatherStation>(s);
+    void update(weak_ptr<SubjectInterface> s) override {
+        auto ws = dynamic_pointer_cast<WeatherStation>(shared_ptr<SubjectInterface>(s));
         if (ws->getHumidity()>0.95 && ws->getPressure()>1){
             forecast="Lluvia";
         }
@@ -110,24 +110,24 @@ public:
 
 class DisplayDevice {
 private:
-    StatisticsObserver statistics_obs;
-    CurrentObserver current_obs;
-    ForecastObserver forecast_obs ;
+    shared_ptr<StatisticsObserver> statistics_obs = make_shared<StatisticsObserver>();
+    shared_ptr<CurrentObserver> current_obs = make_shared<CurrentObserver>();
+    shared_ptr<ForecastObserver> forecast_obs = make_shared<ForecastObserver>();
 public:
     DisplayDevice() = default;
     ~DisplayDevice() = default;
 
     void showDisplay() {
-        cout << "STATISTICS:\n" << statistics_obs.getStatistics() << endl;
-        cout << "CURRENT:\n" << current_obs.getCurrent() << endl;
-        cout << "FORECAST:\n" << forecast_obs.getForecast() << endl;
+        cout << "STATISTICS:\n" << statistics_obs->getStatistics() << endl;
+        cout << "CURRENT:\n" << current_obs->getCurrent() << endl;
+        cout << "FORECAST:\n" << forecast_obs->getForecast() << endl;
     }
 
     vector<shared_ptr<ObserverInterface>> getObserverList() {
         vector<shared_ptr<ObserverInterface>> observers;
-        observers.push_back( shared_ptr<ObserverInterface>(&statistics_obs));
-        observers.push_back( shared_ptr<ObserverInterface>(&current_obs));
-        observers.push_back( shared_ptr<ObserverInterface>(&forecast_obs));
+        observers.push_back( shared_ptr<ObserverInterface>(statistics_obs));
+        observers.push_back( shared_ptr<ObserverInterface>(current_obs));
+        observers.push_back( shared_ptr<ObserverInterface>(forecast_obs));
         return observers;
     }
 };
@@ -136,16 +136,16 @@ int main() {
     auto ws = make_shared<WeatherStation>();
     auto  d1 = make_shared<DisplayDevice>();
     auto  d2 = make_shared<DisplayDevice>();
+    cout << "TEST POINT 1" << endl;
     ws->registerMultipleObserver(d1->getObserverList());
     ws->registerMultipleObserver(d2->getObserverList());
-
+    cout << "TEST POINT 2" << endl;
 
     ws->setHumidity(0.90);
     ws->setPressure(2);
     ws->setTemperature(10);
-
-    ws->removeMultipleObserver(d1->getObserverList());
-
+    cout << "TEST POINT 3" << endl;
+    ws->removeMultipleObserver(d1->getObserverList()); // ACA SE ROMPE
     ws->setHumidity(0.99);
     cout << "DEVICE 1" << endl;
     d1->showDisplay();
